38882c728c8e614e6b5d3b38f5b22c96ba3e9cdf
Hossein Abbasabadi <Hossein.Abbasabadi@outlook.com>
2018-08-03 19:03:40 +0430

    FlightCalculationServiceTests Refactored.

--- FlightSchedule.Domain.Tests.Unit/Services/FlightCalculationServiceTests.cs
+++ FlightSchedule.Domain.Tests.Unit/Services/FlightCalculationServiceTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FlightSchedule.Domain.Model;
 using FlightSchedule.Domain.Services;
 using FlightSchedule.Domain.Shared;
@@ -17,7 +14,28 @@ namespace FlightSchedule.Domain.Tests.Unit.Services
         public void Calculate_should_calculate_flights_based_on_weekly_plan()
         {
             var route = new Route("IKA", "SJC");
-            var reserveSchedule = new ReserveSchedule()
+            var reserveSchedule = CreateReserveSchedule(route);
+            var expectedFlights = CreateExpectedFlights(route);
+
+            var actualFlights = new FlightCalculationService().Calculate(reserveSchedule);
+            actualFlights.Should().BeEquivalentTo(expectedFlights);
+        }
+
+        private static IEnumerable<Flight> CreateExpectedFlights(Route route)
+        {
+            return new List<Flight>()
+            {
+                //TODO: make factory method for flight
+                new Flight(new DateTime(2018,3,19,8,0,0),null,null,route),
+                new Flight(new DateTime(2018,3,21,15,0,0),null,null,route),
+                new Flight(new DateTime(2018,3,26,8,0,0),null,null,route),
+                new Flight(new DateTime(2018,3,28,15,0,0),null,null,route),
+            };
+        }
+
+        private static ReserveSchedule CreateReserveSchedule(Route route)
+        {
+            return new ReserveSchedule()
             {
                 StartReserveDate = new DateTime(2018,03,17),
                 EndReserveDate = new DateTime(2018,04,1),
@@ -28,16 +46,6 @@ namespace FlightSchedule.Domain.Tests.Unit.Services
                     new WeeklyTimetable() { DayOfWeek = DayOfWeek.Wednesday, DepartTime = new TimeSpan(15,0,0)}
                 }
             };
-            var expectedFlights = new List<Flight>()
-            {
-                new Flight(new DateTime(2018,3,19,8,0,0),null,null,route),
-                new Flight(new DateTime(2018,3,21,15,0,0),null,null,route),
-                new Flight(new DateTime(2018,3,26,8,0,0),null,null,route),
-                new Flight(new DateTime(2018,3,28,15,0,0),null,null,route),
-            };
-
-            var actualFlights = new FlightCalculationService().Calculate(reserveSchedule);
-            actualFlights.Should().BeEquivalentTo(expectedFlights);
         }
     }
 }
