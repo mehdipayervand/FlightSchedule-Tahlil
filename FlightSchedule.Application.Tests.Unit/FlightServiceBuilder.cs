4f0d1415e7f6a2d2363b0a000a414b244efc9879
seyed abbas ghomi <seyed.abbas.ghomi@hotmail.com>
2018-08-05 15:30:29 +0430

    Arrange and Act of GenerateFlights_should_calculate_flights_and_save_them Test Method is refactored

--- FlightSchedule.Application.Tests.Unit/FlightServiceBuilder.cs
+++ FlightSchedule.Application.Tests.Unit/FlightServiceBuilder.cs
@@ -1,28 +1,65 @@
-﻿using System;
+﻿using FlightSchedule.Domain.Model;
+using FlightSchedule.Domain.Services;
+using FlightSchedule.Domain.Shared;
+using NSubstitute;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FlightSchedule.Domain.Model;
-using FlightSchedule.Domain.Services;
-using NSubstitute;
 
 namespace FlightSchedule.Application.Tests.Unit
 {
     public class FlightServiceBuilder
     {
-        public IFlightRepository FlightRepository { get; private set; }
-        public IFlightCalculationService FlightCalculationService { get; private set; }
+        #region Fields
+        private int _numberOfFlightsToGenerate;
+        #endregion
+
+        #region Properties
+        public ReserveSchedule ReserveScheduleProp { get; set; }
+        public IFlightCalculationService FlightCalculationServiceProp { get; set; }
+        public List<Flight> CalculatedFlightsProp { get; set; }
+        public IFlightRepository FlightRepositoryProp { get; set; }
+        public FlightService FlightService { get; set; }
+        public int NumberOfFlightsToGenerate
+        {
+            get { return _numberOfFlightsToGenerate; }
+            set
+            {
+                _numberOfFlightsToGenerate = value;
+
+                CalculatedFlightsProp = new FlightsTestListBuilder().GetSomeFlights(_numberOfFlightsToGenerate).ToList();
+                FlightCalculationServiceProp.Calculate(ReserveScheduleProp).Returns(CalculatedFlightsProp);
+                FlightService = new FlightService(FlightRepositoryProp, FlightCalculationServiceProp);
+            }
+        }
+        #endregion
 
         public FlightServiceBuilder()
         {
-            FlightRepository = Substitute.For<IFlightRepository>();
-            FlightCalculationService = Substitute.For<IFlightCalculationService>();
+            ReserveScheduleProp = new ReserveSchedule();
+            FlightCalculationServiceProp = Substitute.For<IFlightCalculationService>();
+            FlightRepositoryProp = Substitute.For<IFlightRepository>();
         }
 
-        public FlightService Build()
+        #region Methods
+        /// <summary>
+        /// This Method will Generate Flights based on given number of Flights
+        /// </summary>
+        /// <param name="numberOfFlights">Number of Flights to generate</param>
+        /// <returns></returns>
+        public FlightServiceBuilder GetSomeFlight(int numberOfFlights)
         {
-            return new FlightService(FlightRepository, FlightCalculationService);
+            this.NumberOfFlightsToGenerate = numberOfFlights;
+            return this;
         }
+
+        /// <summary>
+        /// This Method will generate Fights Schedule
+        /// </summary>
+        /// <returns></returns>
+        public void GenerateFlights()
+        {
+            FlightService.GenerateFlights(ReserveScheduleProp);
+        }
+        #endregion
     }
 }
